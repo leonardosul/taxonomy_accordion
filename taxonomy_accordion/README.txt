22742224ecff9d56fc3811556828d767ec81f89d
Leonard O' Sullivan <leonard@firstnode.com.au>
2014-11-05 21:53:47 +1000

    Initial commit. Adding original D7 Taxonomy Accordion module. taxonomy_accordion 7.x-1.0

---
+++ taxonomy_accordion/README.txt
@@ -0,0 +1,24 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Installation
+
+
+INTRODUCTION
+------------
+
+Current Maintainer: Leonard O' Sullivan <https://drupal.org/user/1362636>
+
+This module provides a block for each taxonomy vocabulary set up in your site.
+The block consists of a H3 tag containing the top level taxonomy term
+for each specific vocabulary, and a list of links to the sub terms, if any
+exist. There is also a link to a page that lists all pages that are aassociated
+with the top level term. Initially the links are hidden, and are shown by
+clicking on the H3 heading.
+
+
+INSTALLATION
+------------
+
+1. Install as usual, see http://drupal.org/node/70151 for further information.
---
+++ taxonomy_accordion/tax_accordion.js
@@ -0,0 +1,41 @@
+/**
+* @file
+* Uses ui.accordion and jquery.cookie sytem libraries.
+*
+* Adds accordion to the selectors passed in from taxonomy_accordion_7.module.
+* Creates a cookie for each accordion selector to remember state betweeen page loads.
+* Cookies expire at the end of the session.
+*/
+
+
+(function($) {
+  Drupal.behaviors.taxonomy_accordion = {
+    attach: function (context, settings) {
+      $.each(settings.taxonomy_accordion.selector_list, function() {
+        var taxSelector = this.toString();
+        var cookieId = taxSelector.match(/\(([^)]+)\)/)[1];
+        cookieId = cookieId.replace(/[|&;$%@"<>()+,]/g, "");
+
+        var cookieName = 'accordionState_' + cookieId;
+        var status = $.cookie(cookieName);
+
+        $(taxSelector).accordion();
+
+        if(status == 'hidden' || status == undefined){
+          $(taxSelector).next().hide();
+          $(taxSelector).click(function() {
+            $(taxSelector).next().toggle("slow");
+            $.cookie(cookieName, 'shown');
+          });
+        }
+        else if (status == 'shown'){
+          $(taxSelector).next().show();
+          $(taxSelector).click(function() {
+            $(taxSelector).next().toggle("slow");
+            $.cookie(cookieName, 'hidden');
+          });
+        }
+      });
+    }
+  };
+})(jQuery);
