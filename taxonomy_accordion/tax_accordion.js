05eebb04b88346193e3a1f8b3383d48336ce79f9
Leonard O' Sullivan <leonard@firstnode.com.au>
2014-11-07 20:36:04 +1000

    Updated code format and added js comments.

--- taxonomy_accordion/tax_accordion.js
+++ taxonomy_accordion/tax_accordion.js
@@ -1,12 +1,11 @@
 /**
 * @file
-* Uses ui.accordion and jquery.cookie sytem libraries.
+* Uses ui.accordion and jquery.cookie system libraries.
 *
-* Adds accordion to the selectors passed in from taxonomy_accordion_7.module.
-* Creates a cookie for each accordion selector to remember state betweeen page loads.
+* Adds accordion to the selectors passed in from taxonomy_accordion.module.
+* Creates a cookie for each accordion selector to remember state between page loads.
 * Cookies expire at the end of the session.
 */
-
 
 (function($) {
   Drupal.behaviors.taxonomy_accordion = {
@@ -17,27 +16,40 @@
         return (obj.textContent || obj.innerText || $(obj).text() || "") == meta[3];
       };
 
+      // For every selector provided by the module.
       $.each(settings.taxonomy_accordion.selector_list, function() {
+        // Grab the selector in a string.
         var taxSelector = this.toString();
+        // Pull the taxonomy name from the selecto.
         var cookieId = taxSelector.match(/\(([^)]+)\)/)[1];
+        // Remove additional double quotes from taxonomy name.
         cookieId = cookieId.replace(/[|&;$%@"<>()+,]/g, "");
 
+        // Add prefix to taxonomy name to create a cookie name.
         var cookieName = 'accordionState_' + cookieId;
+        // Create the cookie.
         var status = $.cookie(cookieName);
 
-        //$(taxSelector).accordion();
-
+        // Add accordion functionality to taxonomy terms.
         $(taxSelector).accordion();
 
+        // If the cookie reads hidden or is not yet defined.
         if(status == 'hidden' || status == undefined){
+          // Hide the elements within that taxonomy parent.
           $(taxSelector).next().hide();
+          // Add a function to show the terms within the taxonomy parent on click.
+          // And to change the cookie to read shown.
           $(taxSelector).click(function() {
             $(taxSelector).next().toggle("slow");
             $.cookie(cookieName, 'shown');
           });
         }
+        // If the cookie reads shown.
         else if (status == 'shown'){
+          // Show the elements within that taxonomy parent.
           $(taxSelector).next().show();
+          // Add a function to hide the terms within the taxonomy parent on click.
+          // And to change the cookie to read hidden.
           $(taxSelector).click(function() {
             $(taxSelector).next().toggle("slow");
             $.cookie(cookieName, 'hidden');
